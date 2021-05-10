c425e4bddf4847d10900e0bad0a6ebc5e46e2394
Théo Moussaoui <pro.moussaoui@gmail.com>
2021-05-10 21:52:30 +0200

    get sorbdev

--- index.js
+++ index.js
@@ -32,4 +32,60 @@ bot.on('ready', async () => {
 })
 
 
-bot.login(process.env.TOKENSOR)
+bot.login(process.env.TOKENTEST)
+
+// let spreadsheetID = '18lwKPEUaRRsMT-4ajR4Y32SZOjg1UT_A4vffSA4B8es'
+// var url = `https://spreadsheets.google.com/feeds/list/${spreadsheetID}/od6/public/values?alt=json`;
+
+// function create(url){
+//     let str=''
+//     let fc=[]
+//     fetch(url,{method: 'GET'})
+//       .then(response => response.json())
+//       .then(json => json.feed.entry)
+//       .then(entry => {
+//           for (const iterator of entry) {
+//               let nom=iterator.gsx$date.$t.split('/')
+//               let date=new Date(20+nom[2],nom[1],nom[0]).getTime()
+//               let ligne=`{"date":${date},"cours":"${iterator.gsx$cours.$t}","prof":"${iterator.gsx$prof.$t}"}`
+//               let obj=JSON.parse(ligne)
+              
+//               if(fc.length==0){
+//                 fc.push(obj) 
+//               }else{
+//                 for (const it of fc) {
+//                   if(obj.date === it.date){
+//                     fc.splice(fc.indexOf(it), 1, obj);
+//                     break;
+//                   }else{
+//                     if (fc.indexOf(it)==fc.length-1) {
+//                       fc.push(obj)
+//                     }
+//                   }
+//                 }
+//               }
+
+
+//           }
+
+//           fc.sort((obj1, obj2) => obj1.date - obj2.date)
+//       })
+    
+//       .catch(err => console.log(err))
+// }
+
+// create(url)
+
+  
+
+/*                                                              
+function email(email){
+    let nom=email.split('@')
+    nom=nom[0].replace("."," ")             Choper nom a partir d'email
+    console.log(nom)
+}
+
+mel = "tom.zigouigoui@tegugst.com"
+
+email(mel)
+*/
