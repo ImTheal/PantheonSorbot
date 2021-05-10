892c67c57c7d6324e30cd77efa8578eb3dfc2c79
MaximePIERRONT <46542631+MaximePIERRONT@users.noreply.github.com>
2021-05-10 17:54:25 +0200

    function database refacto

--- database/databaseFunction/dbExmpleAction.js
+++ database/databaseFunction/dbExmpleAction.js
@@ -1,11 +1,9 @@
-const stuctDb = require('./databaseArchi/initialiseArchi');
+const stuctDb = require('../databaseArchi/initialiseArchi');
 
 const getAllMembersDB = async () => {
   const members = await stuctDb.Member.find({}).exec();
   return members;
 }
-
-
 
 module.exports = {
   getAllMembersDB
