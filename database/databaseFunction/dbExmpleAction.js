5d8e29eebaaa69cdbd0a2d0f18a9395c5af4a3d9
Théo Moussaoui <pro.moussaoui@gmail.com>
2021-05-11 11:48:37 +0200

    skgngn

--- database/databaseFunction/dbExmpleAction.js
+++ database/databaseFunction/dbExmpleAction.js
@@ -1,27 +1,38 @@
-const stuctDb = require('../databaseArchi/initialiseArchi');
+const structDb = require('../databaseArchi/initialiseArchi');
 
 const getAllMembersDB = async () => {
-  const members = await stuctDb.Member.find({}).exec();
+  const members = await structDb.Member.find({}).exec();
   return members;
 }
 
 const createAndGetOneGroupDB = async () => {
-  const group = await stuctDb.Group.findOne({name: 'lala'}).exec();
+  const group = await structDb.Group.findOne({name: 'lala'}).exec();
   return group;
 }
 
 const getAllCalendarsDB = async () => {
-  const group = await stuctDb.Calendar.find({}).exec();
+  const group = await structDb.Calendar.find({}).exec();
   return group;
 }
 
+cosnt getAllClassesFromMember = async(id) => {
+  const member = await structDb.Member.findOne({}).exec();
+  const allAssoGroupRole = await structDb.AssoMemberGroup.find({Member: member});
+  let lala;
+  allAssoGroupRole.forEach(element => {
+    const listIdClasses = element.classes;
+    await  
+  });
+
+}
+
 const getAllRolesDB = async () => {
-  const group = await stuctDb.Calendar.find({}).exec();
+  const group = await structDb.Calendar.find({}).exec();
   return group;
 }
 
 const getRolefromGroupDB = async (id) => {
-  const roles = await stuctDb.AssoGroupRole.find({Group: id}).exec();
+  const roles = await structDb.AssoGroupRole.find({Group: id}).exec();
   console.log(roles);
   return group;
 }
