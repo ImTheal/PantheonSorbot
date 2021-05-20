4b1622c9c35ffb7ac25a106889786471e9a9ec42
Iris Mezieres <iris92mezieres@gmail.com>
2021-05-20 19:04:40 +0200

    quick fix

--- constants/common.js
+++ constants/common.js
@@ -1,6 +1,6 @@
 const COMMON = {
     'CANCELED_OPERATION': 'Opération annulée',
-    'SUCCESSFUL_OPERATION': 'Opération réussite.',
+    'SUCCESSFUL_OPERATION': 'Opération réussie.',
     'FAILED_OPERATION': 'Opération échouée.',
     'TIME_ELAPSED': 'Le temps est écoulé. Veuillez recommencer la manoeuvre.',
     'EXPECTED_FORM': 'Forme attendu'
